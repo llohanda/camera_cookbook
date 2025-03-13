bb8fa84c1316089871e1071328d461d91239871a
Livia Lohanda <training@cjki.org>
2025-03-13 11:28:20 +0900

    Enable saving image to gallery

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:gal/gal.dart';
 
 void main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
@@ -117,6 +118,20 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
   }
 }
 
+enum GalExceptionType {
+  accessDenied,
+  notEnoughSpace,
+  notSupportedFormat,
+  unexpected;
+
+  String get message => switch (this) {
+    accessDenied => 'You do not have permission to access the gallery app.',
+    notEnoughSpace => 'Not enough space for storage.',
+    notSupportedFormat => 'Unsupported file formats.',
+    unexpected => 'An unexpected error has occurred.',
+  };
+}
+
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
 
@@ -130,6 +145,32 @@ class DisplayPictureScreen extends StatelessWidget {
         title: const Text('Display the Picture'),
       ),
       body: Center(child: Image.file(File(imagePath))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await Gal.putImage(imagePath);
+            if (context.mounted) {
+              final snackBar = SnackBar(
+                content: Text('Image saved'),
+                action: SnackBarAction(
+                  label: 'Open Gallery',
+                  onPressed: () async => Gal.open(),
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          } on GalException catch (e) {
+            if (context.mounted) {
+              final snackBar = SnackBar(content: Text(e.type.message));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+            debugPrint(e.type.message);
+          } catch (e) {
+            debugPrint(e.toString());
+          }
+        },
+        child: const Icon(Icons.save_alt),
+      ),
     );
   }
 }
